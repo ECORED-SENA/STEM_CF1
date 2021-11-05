95b4ae008018b3568f43d5e80bf172dff5f4bd7a
Sebastian Giraldo <walter.roas@misena.edu.co>
2021-11-05 17:00:55 -0500

    Update Actividad.vue

--- src/views/Actividad.vue
+++ src/views/Actividad.vue
@@ -7,9 +7,10 @@
       .col-6.col-md-4.col-lg-3.mb-4.mb-md-0
         .tarjeta.tarjeta--azul.h-100.d-flex.align-items-center
           figure
-            img(src="@/assets/template/emparejamiento.svg", alt="Texto que describa la imagen")
+            img(src="@/assets/template/opcion-multiple.svg", alt="Texto que describa la imagen")
       .col-12.col-md-8.col-lg-9
-        .titulo-segundo Reconoce los conceptos
+        .titulo-segundo
+          h2 Reconoce los conceptos
         p.mb-4 Evaluar conceptos estudiados en el componente formativo
         .tarjeta.tarjeta--azul.p-3
           .row.justify-content-around.align-items-center            
